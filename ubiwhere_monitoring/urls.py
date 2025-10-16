67454a6cea9445735bfde1e22f0392a7b04c2ee8
Miguel <miguelcabanasbarros@hotmail.com>
2025-10-16 17:09:46 +0100

    Views Updated Permissions

--- ubiwhere_monitoring/urls.py
+++ ubiwhere_monitoring/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from roads.views import RoadViewSet
+from sensors.views import SensorViewSet
 
 router = DefaultRouter()
 router.register(r"roads", RoadViewSet, basename="roads")
+router.register(r"sensors", SensorViewSet, basename="sensors")
 urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls),
