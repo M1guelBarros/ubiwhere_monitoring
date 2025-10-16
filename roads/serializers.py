183d2733e5578e7573b8ab00b7c551b30b749ccb
Miguel <miguelcabanasbarros@hotmail.com>
2025-10-16 12:14:51 +0100

    Inicial update

---
+++ roads/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Road
+
+
+class RoadSerializer(serializers.ModelSerializer):
+
+    intensity = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Road
+        fields = [
+            "id",
+            "long_start",
+            "long_end",
+            "lat_start",
+            "lat_end",
+            "length",
+            "speed",
+            "updated_time",
+            "intensity",
+        ]
+
+    def get_intensity(self, obj):
+        if obj.speed <= 20:
+            return "elevada"
+        elif obj.speed <= 50:
+            return "média"
+        else:
+            return "baixa"
--- roads/views.py
+++ roads/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Road
+from .serializers import RoadSerializer
 
 # Create your views here.
+
+
+class RoadViewSet(viewsets.ModelViewSet):
+    queryset = Road.objects.all()
+    serializer_class = RoadSerializer
