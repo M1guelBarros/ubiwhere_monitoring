c3834077f0674e06fcaf881de8a1e7e0c21b9209
Miguel <miguelcabanasbarros@hotmail.com>
2025-10-18 18:40:03 +0100

    Swagger

--- ubiwhere_monitoring/urls.py
+++ ubiwhere_monitoring/urls.py
@@ -4,6 +4,8 @@ from rest_framework.routers import DefaultRouter
 from roads.views import RoadViewSet
 from sensors.views import SensorViewSet
 from readings.views import ReadingViewSet
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 router = DefaultRouter()
 router.register(r"roads", RoadViewSet, basename="roads")
@@ -13,4 +15,10 @@ urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
